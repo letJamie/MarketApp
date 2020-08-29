9e1fc344611f1e59465c3ee1ec49af1b60075d89
letJamie <stillbut@naver.com>
2020-08-29 23:15:29 +0900

    Create custom collection view cell

---
+++ MarketApp/CategoryCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryCollectionViewCell.swift
+//  MarketApp
+//
+//  Created by Jamie on 2020/08/29.
+//  Copyright © 2020 Jamie. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+}
